e1dcc04ddefdf2b9e666dade0076129e8a2e86d7
Alexiax3 <89424285+Alexiax3@users.noreply.github.com>
2022-08-23 21:58:26 -0500

    Update factorial_iterativo.c

--- Programas/factorial_iterativo.c
+++ Programas/factorial_iterativo.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 // la cantidad de paso que debe dar este algoritmo est dado por O(2n-1)
 /*
-factRecursion(n) {
+factTail(n, a) {
 	if (n lt 0)
 		return 0
 	else if (n equal 0)
 		return 1
 	else if (n equal 1)
-		return 1
+		return a
 	else
-		return n * fact(n - 1)
+		return factTail(n - 1, n * a)
 }
 */
 
-factRecursion(int n) {
+factTail(int n,int a) {
 	if (n < 0)
 		return 0;
 	else if (n == 0)
 		return 1;
 	else if (n == 1)
-		return 1;
+		return a;
 	else
-		return n * factRecursion(n-1);
+		return factTail(n - 1, n * a);
 }
 
 int main (){
-  unsigned int n=1;
-  factRecursion(n);
+  unsigned int n=3;
+  int a=1;
+  factTail(n,a);
   printf("%d", n);
 	
 }
+
