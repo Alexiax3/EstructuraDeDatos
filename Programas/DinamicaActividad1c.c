e807b8d6b488885d82e11d8e9e8d2c67608b3e91
Alexiax3 <422068044@pcpuma.acatlan.unam.mx>
2022-09-08 17:29:09 -0500

    La verdad no le entendí y lo cree de nuevo

---
+++ Programas/DinamicaActividad1c.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    // pv= used to creat our first array
+    int *pv;  
+    // i = used by for
+    int i;
+     pv = (int*)malloc(10 * sizeof(int));
+     
+    // creating the arry with pars 
+     printf("\n\n\t PARS ARRAY");
+        for(i=0; i<10; i++) {  
+	        pv[i] = 2*i;
+	        //printf("\n %d", pv[10-i]);
+        }
+    
+    // printng array 
+    
+    i=0;
+        for (i=0; i<=9; i++){
+            printf("\n %d", pv[9-i]);
+        }
+        
+    // el numero de cuenta es 422068044 y su ultimo digito es 4
+        pv[4]=-1;
+        
+      printf("\n\n\t NEW ARRAY");   
+    i=1;
+        for (i=1; 10>i; i++){
+            printf("\n %d", pv[i]);
+        }   
+        
+    free (pv);
+
+    return 0;
+}
+
