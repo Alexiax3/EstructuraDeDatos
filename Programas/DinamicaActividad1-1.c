36b1175fabf265e3d253811e35d4045fd2550253
Alexiax3 <422068044@pcpuma.acatlan.unam.mx>
2022-09-08 23:57:30 -0500

    ah, creo que ya le estamos entendiendo

--- Programas/DinamicaActividad1-1.c
+++ Programas/DinamicaActividad1-1.c
@@ -1,38 +1,51 @@
+#include <stdlib.h> 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main()
-{
-    int i;
-    int a, j;
-    int rows = 10;  
-    int **matrix ;
-    *matrix= (int **) malloc(rows * sizeof(int *));  
-    
-        if (*matrix == NULL)
-	        return -1;
-        
-        for (i = 0; i < rows ; i++) {  
-            printf("\n\t%d, digit: ",i);
-		    scanf("%d", &a);
-        	matrix[i] = (int *) malloc(a * sizeof(int)); 
-        	
-        	
-        	 if (matrix == NULL)
-	            return -1;
-        	
-        	for(j = 0; j < a; j++){
-        	    //memset(void *str, int c, size_t n)
-        	    memset(matrix,a , a);
-        	    //matrix[j]=a;
-        	}
-        	
-        }
-             
-        for (i=0;  i < rows; i++){
-            free (matrix[i]); 
-        }
-        free (*matrix);
-}
+int main() 
+{ 
 
+	int filas = 10;
+	int columnas ;	
+	int **x;	
+    int *n;
+	int i;	 // Recorre filas 
+	int j;	 // Recorre columnas 
+    int a;
+    n = (int *)malloc(filas*sizeof(int*)); 
+    if (n == NULL)
+	return -1;
+	
+	// Reserva de Memoria 
+	x = (int **)malloc(filas*sizeof(int*)); 
+	if (x == NULL)
+	return -1;
+	
+	for (i=0;i<filas;i++) {
+	    printf("\n\t%d, digit: ",i);
+		scanf("%d", &a);
+	    x[i] = (int*)malloc(a*sizeof(int)); 
+	    if (x[i] == NULL)
+    	return -1;
+    	
+	    n[i]=a;
+	    // Damos Valores a la Matriz 
+	    for(j=0;j<a;j++){
+	        x[i][j]=a;
+
+	    }
+	}
+
+	// Dibujamos la Matriz en pantalla 
+	for (i=0; i<filas; i++) 
+	{ 
+		printf("\n------------------------------------------------------------------------------------------------------------------------------------\n"); 
+		for (j=0; j<n[i]; j++) 
+		if (n[i!=0])
+			printf("\t%d\t|", x[i][j] ); 
+	} 
+	
+	for(i=0; i<filas; i++)
+    free
+	
+	return 0; 
+} 
