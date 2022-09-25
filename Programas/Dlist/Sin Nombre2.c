e90d3ac404fd385179520167f92f1d0aafe77b13
Alexiax3 <422068044@pcpuma.acatlan.unam.mx>
2022-09-24 23:42:42 -0500

    LISTO, TOMO TIEMPO, PERO SE LOGRÓ. Se ven mejoras aunque hay muchisismo camino por recorrer. Vamos bien corazon, no te deseperes.

--- Programas/Dlist/Sin Nombre2.c
+++ Programas/Dlist/Sin Nombre2.c
@@ -15,7 +15,7 @@ static void print_Dlist (const DList *list) {
 
     while (1) {
         data = dlist_data(node);
-        fprintf(stdout, "Dlist.node[%03d]=%03d,%p -> %p -> %p \n", i, *data, node->prev, node, node->next);
+        fprintf(stdout, "Dlist.node[%03d]=%010d,%p -> %p -> %p \n", i, *data, node->prev, node, node->next);
 
         i++;
 
@@ -32,13 +32,13 @@ static void print_Dlist (const DList *list) {
 int main (int argc, char **argv) {
     DList list;
     DListNode *node, *a;
-    int *data, i;
+    int *data,*data2, i, j;
 
     // Initialize the linked list
     dlist_init(&list, free);
     
     // Fill the linked list
-
+	printf("\n\n\n\t\t\tLISTA INICIAL");
     for (i = 1; i<11  ; i++ ){
     	//a=0;
 
@@ -72,27 +72,63 @@ int main (int argc, char **argv) {
 	print_Dlist(& list);
 
 	
-    	printf("\n\n\n\t\t\tREMOVER COLA");
+    printf("\n\n\n\t\t\tREMOVER COLA");
 	
 	   	if (dlist_remove(&list, list.tail, (void**)&data) != 0)
         return 1;
 	//}
 	print_Dlist(& list);
 
-    printf("\n\n\n\t\t\tREMOVER 5-7");
+	printf("\n\n\n\t\t\tINSERTAR CUENTA EN EL MEDIO");
+	
+	j=list.size/2;
 	node=list.head;
-		for(i=0;i<8;i++){
-			
-			if(i<7 && i>3){
+	i=0;
+	do{
+		node=node->next;
+		i++;
+	}while(j>i);
+				    
+			*data=422068022;
+    		if (dlist_ins_prev(&list,node,data) != 0)
+        		return 1;
+		
+		print_Dlist(& list);
+
+	
+	printf("\n\n\n\t\t\tINSERTAR CUENTA EN TAIL");
+    if (dlist_ins_next(&list, list.tail,data) != 0)
+        return 1;
+	print_Dlist(& list);
+	
+	printf("\n\n\n\t\t\tINSERTAR SUMA DE CUENTAS EN HEAD");
+	if ((data2 = (int *)malloc(sizeof(int))) == NULL){
+        	//a+=5;
+			return 5;
+		}
+	j=2*422068022;
+	*data2=j;
+    if (dlist_ins_prev(&list, list.head,data2) != 0)
+        return 1;
+	print_Dlist(& list);
+	
+    printf("\n\n\n\t\t\tREMOVER 5-7");
+	j=0;
+	while(j<3){
+	node=list.head;
+		for(i=0;i<5;i++){
+			node=node->next;
+			if(i==4){
 				if (dlist_remove(&list,node , (void**)&data) != 0)
         			return 1;
-			}
-		node=node->next;
+        			break;
+			}	
 		}
-	   	
-	//}
+		j++;
+	}
 	print_Dlist(& list);
-    
+	
+	
     //cambio de cabeza por 9
     
     /*
